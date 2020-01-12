2c7831bd9e84ffd3d79e7071c3f5f52c68ee17e3
Ethan Snow <bombitmanbomb@gmail.com>
2020-01-12 12:35:56 -0500

    Create Friend.js

---
+++ API/Cloud/Friend.js
@@ -0,0 +1,13 @@
+class Friend {
+    constructor() {
+        this.FriendUserId
+        this.OwnerId
+        this.FriendUsername
+        this.FriendStatus
+        this.IsAccepted
+        this.UserStatus
+        this.LatestMessageTime
+        this.Profile
+    }
+}
+module.exports = { Friend }
