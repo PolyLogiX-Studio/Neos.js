6a2d84a03c3ff0b7706a1869d0a793c3923faac9
Ethan Snow <bombitmanbomb@gmail.com>
2020-01-12 12:57:56 -0500

    Update CloudVariable.js

--- API/Cloud/CloudVariable.js
+++ API/Cloud/CloudVariable.js
@@ -1,20 +1,16 @@
 class CloudVariable {
     constructor(){
-        this.ownerId
-        this.path
-        this.value
+        this.VariableOwnerId = new String()
+        this.Path = new String()
+        this.Value = new String()
     }
-    get VariableOwnerId(){return this.ownerId}
-    set VariableOwnerId(value){this.ownerId = value}
-    get Path(){return this.path}
-    set Path(value) {this.path = value}
-    get Value(){return this.value}
-    set Value(value){this.value = value}
-    //GetDefinitionPath(){}
     static GetDefinitionPath(path,ownerId,subpath){
         let length = path.indexOf('.')
         ownerId.value = path.substring(0,length)
         subpath.value = path.substring(length + 1)
+    }
+    GetDefinitionPath(ownerId,subpath){
+        CloudVariable.GetDefinitionPath(this.Path,ownerId,subpath)
     }
 }
 module.exports = {CloudVariable}
