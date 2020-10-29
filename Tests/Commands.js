e2375c18b7c4413633ea4f3ad18cad8e52eb0e8c
Ethan Snow <bombitmanbomb@gmail.com>
2020-10-29 06:34:10 -0400

    Testing

--- Tests/Commands.js
+++ Tests/Commands.js
@@ -16,29 +16,60 @@ const Commands = new CommandExtended(new CommandHandler(Neos, "No Command, Try /
 Commands.Add("ping", h => h.Reply("pong!"), "Ping Pong!")
 
 Neos.on("messageReceived", Commands.Run)
-Neos.Login(process.env.neosLogin, process.env.neosPassword, undefined, uuidv4())
+Neos.Login(process.env.NEOS_LOGIN, process.env.NEOS_PASSWORD, undefined, uuidv4())
 Neos.on("login", () => {
-  console.log(process.env.neosLogin + " Logged in")
+  console.log(process.env.NEOS_LOGIN + " Logged in")
 })
 
 // TESTER ACCOUNT
 Neos2.on("login", () => {
-  console.log(process.env.neosLoginSecond + " Logged in")
-  Neos2.SendTextMessage(process.env.neosLogin, "/test")
-  Neos2.SendTextMessage(process.env.neosLogin, "/commands")
-  Neos2.SendTextMessage(process.env.neosLogin, "/help ping")
-  Neos2.SendTextMessage(process.env.neosLogin, "/ping")
+  TEST.Test()
+  console.log(process.env.NEOS_LOGIN_SECOND + " Logged in")
 })
+
 Neos2.on("messageReceived", (m) => {
-  //if (m.SenderId == Neos2.CurrentUser.Id) return
+  if (m.SenderId != process.env.NEOS_LOGIN) return
   console.log(m.SenderId + ":" + m.Content)
+  TEST.Next()
 })
-setTimeout(() => Neos2.Login(process.env.neosLoginSecond, process.env.neosPasswordSecond, undefined, uuidv4() /* Machine ID */ ), 2000)
+setTimeout(() => Neos2.Login(process.env.NEOS_LOGIN_SECOND, process.env.NEOS_PASSWORD_SECOND, undefined, uuidv4() /* Machine ID */ ), 2000)
+setTimeout(() => {
+  process.exit(1)
+}, 25000);
 process.on('SIGINT', function () {
   try {
     console.log("Logging Out All Accounts")
     Neos.Logout(true);
     Neos2.Logout(true)
   } catch (e) {}
-  process.exit();
+  process.exit(0);
 });
+const TEST = {
+  tests:[],
+  Create(test){
+    this.tests.push(test)
+  },
+  Test()  {
+    let test = this.tests.shift()
+    test()
+  },
+  Next(){
+    console.log("Next Test")
+    var test = this.tests.shift()
+    if (!test){
+      try {
+        console.log("Logging Out All Accounts")
+        Neos.Logout(true);
+        Neos2.Logout(true)
+      } catch (e) {}
+      return process.exit(0)
+    }
+    console.log(test)
+    test()
+  }
+}
+
+TEST.Create(()=>{console.log("Send /test");Neos2.SendTextMessage(process.env.NEOS_LOGIN, "/test")})
+TEST.Create(()=>{console.log("Send /commands");Neos2.SendTextMessage(process.env.NEOS_LOGIN, "/commands")})
+TEST.Create(()=>{console.log("Send /help ping");Neos2.SendTextMessage(process.env.NEOS_LOGIN, "/help ping")})
+TEST.Create(()=>{console.log("Send /ping");Neos2.SendTextMessage(process.env.NEOS_LOGIN, "/ping")})
