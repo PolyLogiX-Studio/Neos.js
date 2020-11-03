724403e1227bc1138adde8d368c3c5ed1fe7a48f
Ethan Snow <bombitmanbomb@gmail.com>
2020-11-03 08:32:52 -0500

    Formatting

--- Tests/Headless.js
+++ Tests/Headless.js
@@ -2,8 +2,8 @@ if (!process.env.dotenv) require("dotenv").config();
 const { v4: uuidv4 } = require("uuid");
 const NEOS = require("../Neos");
 const Neos = new NEOS({
-  updateInterval: 3,
-  StatusInterval:0
+	updateInterval: 3,
+	StatusInterval: 0,
 });
 
 const CommandHandler = require("../Plugins/CommandHandler");
@@ -12,44 +12,46 @@ const HeadlessInterface = require("../Plugins/HeadlessInterface");
 
 // BOT ACCOUNT
 const Options = {
-  Prefix: "/",
+	Prefix: "/",
 }; // CommandExtended options
 const Commands = new CommandExtended(
-  new CommandHandler(Neos, "No Command, Try /commands"),
-  Options
+	new CommandHandler(Neos, "No Command, Try /commands"),
+	Options
 );
 
 Neos.on("messageReceived", Commands.Run);
 Neos.Login(
-  process.env.NEOS_LOGIN,
-  process.env.NEOS_PASSWORD,
-  undefined,
-  uuidv4()
+	process.env.NEOS_LOGIN,
+	process.env.NEOS_PASSWORD,
+	undefined,
+	uuidv4()
 );
 
 Neos.on("login", () => {
-  Neos.AddFriend("U-bombitmanbomb"); // Ensure Friends
-  const NeosVR = new HeadlessInterface(
-    `F:/SteamLibrary/steamapps/common/NeosVR/HeadlessClient`, null, {Events:true}
-  );
-  function runCommand(h, s, a) {
-    NeosVR.Send(a.join(" ")).then((response) => h.Reply(response));
-  }
-  Commands.Add("run", runCommand, "", ["U-bombitmanbomb"]);
-  NeosVR.Events.on("HeadlessResponse", (data) => {
-    if (data.startsWith("World running...")) {
-      NeosVR.Send("invite bombitmanbomb").then(() => {
-        NeosVR.Send('message bombitmanbomb "this is a test"');
-      });
-    }
-  });
+	Neos.AddFriend("U-bombitmanbomb"); // Ensure Friends
+	const NeosVR = new HeadlessInterface(
+		"F:/SteamLibrary/steamapps/common/NeosVR/HeadlessClient",
+		null,
+		{ Events: true }
+	);
+	function runCommand(h, s, a) {
+		NeosVR.Send(a.join(" ")).then((response) => h.Reply(response));
+	}
+	Commands.Add("run", runCommand, "", ["U-bombitmanbomb"]);
+	NeosVR.Events.on("HeadlessResponse", (data) => {
+		if (data.startsWith("World running...")) {
+			NeosVR.Send("invite bombitmanbomb").then(() => {
+				NeosVR.Send("message bombitmanbomb \"this is a test\"");
+			});
+		}
+	});
 });
 
 process.on("SIGINT", function () {
-  try {
-    console.log("Logging Out All Accounts");
-    Neos.Logout(true);
-    Neos2.Logout(true);
-  } catch (e) {}
-  process.exit(0);
+	try {
+		console.log("Logging Out All Accounts");
+		Neos.Logout(true);
+		Neos2.Logout(true);
+	} catch (e) {}
+	process.exit(0);
 });
