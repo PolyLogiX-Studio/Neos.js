8ded639a5bc3a6e33d60670a2df72dedb926f186
Ethan Snow <bombitmanbomb@gmail.com>
2020-11-04 03:31:39 -0500

    Update TransactionMessage.js

--- API/TransactionMessage.js
+++ API/TransactionMessage.js
@@ -13,12 +13,10 @@ class TransactionMessage {
 	constructor($b) {
 		if (!$b) $b = {};
 		this.Token = $b.token;
-		this.FromUserId = $b.fromUserId;
-		this.ToUserId = $b.toUserId;
+		this.RecipientId = $b.recipientId;
 		this.Amount = $b.amount;
 		this.Comment = $b.comment;
 		this.TransactionType = $b.transactionType || null;
-		this.Anonymous = $b.anonymous;
 	}
 }
 module.exports = {
