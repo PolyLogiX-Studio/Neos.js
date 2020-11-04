36265ede079fbeae2edd9616b2ee1a99145e02dc
Ethan Snow <bombitmanbomb@gmail.com>
2020-11-04 08:27:24 -0500

    Update Headless.js

--- Tests/Headless.js
+++ Tests/Headless.js
@@ -26,7 +26,7 @@ Neos.Login(
 	undefined,
 	uuidv4()
 );
-
+//Start NEOS Session on NeosJS Login
 Neos.on("login", () => {
 	Neos.AddFriend("U-bombitmanbomb"); // Ensure Friends
 	const NeosVR = new HeadlessInterface(
@@ -34,24 +34,51 @@ Neos.on("login", () => {
 		null,
 		{ Events: true }
 	);
-	function runCommand(h, s, a) {
-		NeosVR.Send(a.join(" ")).then((response) => h.Reply(response));
-	}
-	Commands.Add("run", runCommand, "", ["U-bombitmanbomb"]);
-	NeosVR.Events.on("HeadlessResponse", (data) => {
-		if (data.startsWith("World running...")) {
-			NeosVR.Send("invite bombitmanbomb").then(() => {
-				NeosVR.Send("message bombitmanbomb \"this is a test\"");
+	Commands.Add(
+		"run",
+		function (h, s, a) {
+			NeosVR.Send(a.join(" ")).then((response) => h.Reply(response));
+		},
+		"",
+		["U-bombitmanbomb"]
+	);
+	NeosVR.on("error", (err) => {
+		console.error(err); // Error Handling Required! Will Throw if no error listener and something goes wrong
+	});
+	NeosVR.on("ready", (sessionId) => {
+		// Headless is ready, Emits Session ID
+		Neos.SendTextMessage(
+			"U-bombitmanbomb",
+			`New session started: ${sessionId}`
+		);
+		setInterval(() => {
+			Neos.CloudXInterface.GetSession(sessionId).then((session) => {
+				console.log(session);
 			});
-		}
+		}, 10000);
+		setTimeout(() => {
+			NeosVR.Send("invite bombitmanbomb").then((response) => {
+				console.log(response); //Unless error (ei. Not Friend) returns Invite Sent
+			});
+			NeosVR.Send("invite bombitmanbomb").then((response) => {
+				console.log(response);
+				//This will error!, .Send must wait for a previous command to finish using .then() (For Now, Queue in the works)
+				// To check if you can send a message, check value of .CanSend before running Send
+			});
+			NeosVR.Send("invite bombitmanbomb").then((response) => {
+				console.log(response); //This will also error!
+			});
+		}, 1000); // Library can only handle 1 response at a time (For now) I have a plan to change this
 	});
 });
 
+//on CRTL+C Logout safely
 process.on("SIGINT", function () {
 	try {
 		console.log("Logging Out All Accounts");
 		Neos.Logout(true);
-		Neos2.Logout(true);
-	} catch (e) {}
+	} catch (e) {
+		process.exit(1);
+	}
 	process.exit(0);
 });
