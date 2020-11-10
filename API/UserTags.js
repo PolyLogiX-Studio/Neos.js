f9333bf562e896d4f0e446c6ff7c4b95de16bdcc
Ethan Snow <bombitmanbomb@gmail.com>
2020-11-10 13:07:25 -0500

    Update UserTags.js

--- API/UserTags.js
+++ API/UserTags.js
@@ -10,14 +10,26 @@ class UserTags {
 	static get NeosModerator() {
 		return "neos moderator";
 	}
-	static get NeosCommunityManager() {
-		return "neos community manager";
+	static get NeosEast() {
+		return "neos east";
+	}
+	static get NeosSpain() {
+		return "neos spain";
+	}
+	static get HTC_Vive() {
+		return "htc vive";
+	}
+	static get ExternalPatreon() {
+		return "external patreon";
 	}
 	static get DiagnoseRecordSync() {
 		return "diagnose record sync";
 	}
 	static get HearingImpaired() {
 		return "hearing impaired";
+	}
+	static get ColorBlind() {
+		return "color blind";
 	}
 	static get Potato() {
 		return "potato";
@@ -27,6 +39,25 @@ class UserTags {
 	}
 	static get Java() {
 		return "java";
+	}
+	static CustomBadge(neosDb, pointFiltering) {
+		let str = "custom badge:" + CloudXInterface.NeosDBSignature(neosDb);
+		if (pointFiltering) str += ".point";
+		return str;
+	}
+	static GetCustomBadge(badge, pointFiltering) {
+		if (!badge.startsWith("custom badge:")) {
+			pointFiltering.Out = false;
+			return null;
+		}
+		badge = badge.substr("custom badge:".length).trim();
+		pointFiltering.Out = badge.includes(".point");
+		return new Uri("neosdb:///" + badge.trim());
+	}
+	static GetCustom3DBadge(badge) {
+		if (!badge.startsWith("custom 3D badge:")) return null;
+		badge = badge.substr("custom 3D badge:".length).trim();
+		return new Uri("neosrec:///" + badge.trim());
 	}
 	static get NCC_Participant() {
 		return "ncc participant";
@@ -40,22 +71,41 @@ class UserTags {
 	static get NCC_Silver() {
 		return "ncc silver";
 	}
-	static get NeosEast() {
-		return "neos east";
+	static get MMC_Participant() {
+		return "mmc participant";
 	}
-	static CustomBadge(neosDb, pointFiltering) {
-		let str = "custom badge:" + CloudXInterface.NeosDBSignature(neosDb);
-		if (pointFiltering) str += ".point";
-		return str;
+	static get MMC_Cow() {
+		return "mmc cow";
 	}
-	static GetCustomBadge(badge, pointFiltering) {
-		if (!badge.startsWith("custom badge:")) {
-			pointFiltering.Out = false;
-			return null;
-		}
-		badge = badge.substr("custom badge:".length).trim();
-		pointFiltering = badge.includes(".point");
-		return new Uri("neosdb:///" + badge.trim());
+	static get MMC_Lips() {
+		return "mmc lips";
+	}
+	static get MMC_World1st() {
+		return "mmc world 1st";
+	}
+	static get MMC_World2nd() {
+		return "mmc world 2nd";
+	}
+	static get MMC_World3rd() {
+		return "mmc world 3rd";
+	}
+	static get MMC_Avatar1st() {
+		return "mmc avatar 1st";
+	}
+	static get MMC_Avatar2nd() {
+		return "mmc avatar 2nd";
+	}
+	static get MMC_Avatar3rd() {
+		return "mmc avatar 3rd";
+	}
+	static get MCC_Other1st() {
+		return "mmc other 1st";
+	}
+	static get MCC_Other2nd() {
+		return "mmc other 2nd";
+	}
+	static get MCC_Other3rd() {
+		return "mmc other 3rd";
 	}
 }
 module.exports = {
