adf92f4b711e2c4fad17d38a050b583a15d50cff
Ethan Snow <bombitmanbomb@gmail.com>
2020-11-14 09:51:12 -0500

    🐛 FIX: Read Undefined on response

--- API/CurrencyRates.js
+++ API/CurrencyRates.js
@@ -7,8 +7,8 @@ class CurrencyRates {
 				? $b.rates
 				: ((rates) => {
 					let rate = new Dictionary();
-					for (let r in rates.rates) {
-						rate.Add(r, rates.rates[r]);
+					for (let r in rates) {
+						rate.Add(r, rates[r]);
 					}
 					return rate;
 					//eslint-disable-next-line no-mixed-spaces-and-tabs
