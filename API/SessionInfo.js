58080a5db06e6ad2e03453c822677a994a33231f
Ethan Snow <bombitmanbomb@gmail.com>
2020-11-14 07:43:11 -0500

    👌 IMPROVE: Refactor SessionInfo

--- API/SessionInfo.js
+++ API/SessionInfo.js
@@ -2,116 +2,141 @@ const { List } = require("./List");
 const { Type } = require("./Type");
 const { Uri } = require("./Uri");
 const { HashSet } = require("./HashSet");
+const { SessionUser } = require("./SessionUser");
 class SessionInfo {
-	/**
-	 *Creates an instance of SessionInfo.
-	 * @param {{
-	 * name:String,
-	 * description:String,
-	 * tags:HashSet<String>,
-	 * sessionId:String,
-	 * hostUserId:String,
-	 * hostMachineId:String,
-	 * hostUsername:String,
-	 * compatibilityHash:String,
-	 * neosVersion:String,
-	 * headlessHost:Boolean,
-	 * url:String,
-	 * sessionURLs:List<String>,
-	 * sessionUsers:List<SessionUser>,
-	 * thumbnail:String,
-	 * joinedUsers:Number,
-	 * activeUsers:Number,
-	 * maxUsers:Number,
-	 * mobileFriendly:Boolean,
-	 * sessionBeginTime:Date,
-	 * lastUpdate:Date,
-	 * awaySince?:Date,
-	 * accessLevel:SessionAccessLevel
-	 * }} $b
-	 * @memberof SessionInfo
-	 */
 	constructor($b) {
 		if (!$b) $b = {};
 		this.Name = $b.name;
 		this.Description = $b.description;
-		this.Tags = new HashSet($b.tags);
+		this.CorrespondingWorldId = $b.correspondingWorldId;
+		this.Tags = $b.tags
+			? $b.tags instanceof HashSet
+				? $b.tags
+				: new HashSet($b.tags)
+			: null;
 		this.SessionId = $b.sessionId;
 		this.HostUserId = $b.hostUserId;
-		this.HostMachineId = $b.hostMachineId;
 		this.HostUsername = $b.hostUsername;
-		this.CompatibilityHash = $b.compatibilityHash;
+		this.CompatabilityHash = $b.compatabilityHash;
+		this.UniverseId = $b.universeId;
 		this.NeosVersion = $b.neosVersion;
 		this.HeadlessHost = $b.headlessHost;
-		this.LegacySessionURL = $b.url || null; //LEGACY
-		let SessionURLs = $b.sessionURLs;
-		if (Type.Get(SessionURLs) === "List") this.SessionURLs = SessionURLs;
-		if (Type.Get(SessionURLs) === "Array")
-			this.SessionURLs = List.ToList(SessionURLs);
-		let SessionUsers = $b.sessionUsers;
-		if (Type.Get(SessionUsers) === "List") this.SessionUsers = SessionUsers;
-		if (Type.Get(SessionUsers) === "Array")
-			this.SessionUsers = List.ToList(SessionUsers);
-		this.Thumbnail = $b.thumbnail;
-		this.JoinedUsers = $b.joinedUsers;
-		this.ActiveUsers = $b.activeUsers;
-		this.MaximumUsers = $b.maxUsers;
-		this.MobileFriendly = $b.mobileFriendly;
-		this.SessionBeginTime = $b.sessionBeginTime;
-		this.LastUpdate = $b.lastUpdate;
-		this.AwaySince = $b.awaySince;
-		this.AccessLevel = $b.accessLevel;
-		this.IsLAN = new Boolean();
+		this.SessionURLs = $b.sessionURLs
+			? $b.sessionURLs instanceof List
+				? $b.sessionURLs
+				: List.ToList($b.sessionURLs)
+			: null;
+		this.SessionUsers = $b.sessionUsers
+			? $b.sessionUsers instanceof List
+				? $b.sessionUsers
+				: ((users) => {
+					let userList = new List();
+					for (user of users) {
+						userList.Add(new SessionUser(user));
+					}
+					return userList;
+				  })($b.sessionUsers)
+			: new List();
+			this.Thumbnail = $b.thumbnail
+			this.JoinedUsers = $b.joinesUsers
+			this.ActiveUsers = $b.activeUsers
+			this.MaximumUsers = $b.maximumUsers
+			this.MobileFriendly = $b.mobileFriendly
+			this.SessionBeginTime = $b.sessionBeginTime
+			this.LastUpdate = $b.lastUpdate
+			this.AwaySince = $b.awaySince //Can be Null
+			this.AccessLevel = $b.accessLevel //Enum
+
 	}
-	/**
-	 *
-	 *
-	 * @returns {List<Uri>}
-	 * @memberof SessionInfo
-	 */
-	GetSessionURLs() {
-		if (this.SessionURLs != null)
-			return List.ToList(
-				this.SessionURLs.filter((str) => {
-					return str;
-				}).map((str) => new Uri(str))
-			);
-		let uriList = new List();
-		if (this.LegacySessionURL != null)
-			uriList.Add(new Uri(this.LegacySessionURL));
-		return uriList;
+	get IsOnLAN(){
+		return this.LAN_URL != null
 	}
-	/**
-	 *
-	 * @readonly
-	 * @memberof SessionInfo
-	 */
-	get HasEnded() {
-		if (this.SessionURLs == null || this.SessionURLs.length === 0)
-			return this.LegacySessionURL == null;
-		return false;
+	get HasEnded(){
+		return this.SessionURLs == null || this.SessionURLs.Count === 0;
 	}
-	/**
-	 *
-	 * @param {SessionInfo} other
-	 * @returns {Boolean}
-	 * @memberof SessionInfo
-	 */
-	IsSame(other) {
-		if (
-			!(this.Name === other.Name) ||
-			!(this.Description === other.Description) ||
-			!this.Tags.IsSame(other.Tags) ||
-			!(this.SessionId === other.SessionId) ||
-			!(this.HostUserId === other.HostUserId) ||
-			!(this.HostMachineId === other.HostMachineId) ||
-			!(this.HostUsername === other.HostUsername) ||
-			!(this.CompatibilityHash === other.CompatibilityHash) ||
-			!(this.NeosVersion === other.NeosVersion) ||
-			this.HeadlessHost !== other.HeadlessHost
-		)
-			return false;
-		return true;
+	SetEnded(){
+		this.SessionURLs = null
+	}
+	CopyLAN_Data(source){
+		this.LAN_URL = source.LAN_URL
+		this.LastLAN_Update = source.LastLAN_Update
+		if (this.LAN_URL == null) return;
+		if (this.SessionURLs == null)
+			this.SessionURLs = new List()
+		this.SessionURLs.Add(this.LAN_URL)
+	}
+	GetSessionURLs(){
+		let urls = new List()
+		for (url in this.SessionURLs){
+			urls.Add(new Uri(url))
+		}
+		return urls
+	}
+	get NormalizedSessionId() {
+		if (this.SessionId) return this.SessionId.toLower();
+		return null;
+	}
+
+	get MAX_NAME_LENGTH() {
+		return 256;
+	}
+	get MAX_DESCRIPTION_LENGTH() {
+		return 16384;
+	}
+	get MAX_TAG_LENGTH() {
+		return 128;
+	}
+	get MAX_TAGS() {
+		return 256;
+	}
+	get MAX_ID_LENGTH() {
+		return 128;
+	}
+	get MAX_URL_LENGTH() {
+		return 256;
+	}
+	SessionInfo(sessionId) {
+		this.SessionId = sessionId;
+		this.LastUpdate = new Date();
+	}
+	static IsCustomSessionId(sessionId) {
+		return sessionId.startsWith("S-U-");
+	}
+	static GetCustomSessionOwnerId(sessionId) {
+		var num = sessionId.indexOf(":");
+		if (!~num)
+			throw new Error("Invalid custom sessionId! Make sure it's valid first.");
+		return sessionId.substr(2, num - 2);
+	}
+	static IsValidSessionId(sessionId) {
+		if (sessionId == null || sessionId.trim() == "") return false;
+		for (let c of sessionId) {
+			if (!/[0-9]/.test(c)) {
+				if (/[a-zA-Z]/.test(c)) {
+					if (c > "\u007F") return false; // No Unicode
+				} else if (c != "-" && c != ":" && c != "_") return false; // No Tags
+			}
+		}
+		return (
+			!sessionId.startsWith("U-") &&
+			(!SessionInfo.IsCustomSessionId(sessionId) || sessionId.indexOf(":") >= 0)
+		);
+	}
+	IsSame(other){
+		//TODO
+		return this.Name == other.Name
+	}
+	HasTag(tag){
+		return this.Tags != null && this.Tags.Contains(tag)
+	}
+	get IsValid(){
+		//TODO
+	}
+	Trim(){
+		/TODO
+	}
+	ToString(){
+		return "SessionInfo. Id "+this.SessionId
 	}
 }
 module.exports = {
