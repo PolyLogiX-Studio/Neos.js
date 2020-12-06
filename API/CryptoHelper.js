1fe3fa5a2777baf917cc1ca733ff250217e45e63
Ethan Snow <bombitmanbomb@gmail.com>
2020-12-05 20:54:36 -0500

    📦 NEW: CryptoHelper

--- API/CryptoHelper.js
+++ API/CryptoHelper.js
@@ -1,4 +1,11 @@
-class CryptoHelper {}
+const crypto = require("crypto");
+// eslint-disable-next-line no-unused-vars
+const SHA256 = require("crypto-js/sha256");
+class CryptoHelper {
+	static GenerateCryptoToken() {
+		return crypto.randomBytes(16).toString().replace("-", "");
+	}
+}
 module.exports = {
 	CryptoHelper,
 };
