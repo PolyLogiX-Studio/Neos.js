1c189ed34701d991a2f94f7e9e0127985fd904ad
Ethan Snow <bombitmanbomb@gmail.com>
2020-12-08 03:43:57 -0500

    LGTM

--- API/CryptoHelper.js
+++ API/CryptoHelper.js
@@ -1,6 +1,6 @@
 const crypto = require("crypto");
 // eslint-disable-next-line no-unused-vars
-const SHA256 = require("crypto-js/sha256");
+//const SHA256 = require("crypto-js/sha256");
 class CryptoHelper {
 	static GenerateCryptoToken() {
 		return crypto.randomBytes(16).toString().replace("-", "");
