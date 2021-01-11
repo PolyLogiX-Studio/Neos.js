7186d6b063adc1b291b073982d5682b4cb6ab2a7
Ethan Snow <bombitmanbomb@gmail.com>
2021-01-11 02:43:32 -0500

    📦 NEW: Parser for Uri Type

--- API/Uri.js
+++ API/Uri.js
@@ -20,7 +20,7 @@ class Uri {
 			enumerable: false,
 		});
 		Object.defineProperty(this, "_raw", {
-			value: URI.parse(url),
+			value: uri_parse(url),
 			enumerable: false,
 		});
 		let path = this._raw.path.split("/");
@@ -60,6 +60,58 @@ class Uri {
 		return encodeURI(dat);
 	}
 }
+
+//Built refrencing uri-js
+const URI_PARSE = /^(?:([^:/?#]+):)?(?:\/\/((?:([^/?#@]*)@)?(\[[^/?#\]]+\]|[^/?#:]*)(?::(\d*))?))?([^?#]*)(?:\?([^#]*))?(?:#((?:.|\n|\r)*))?/i;
+const NO_MATCH_IS_UNDEFINED = "".match(/(){0}/)[1] === undefined;
+
+function uri_parse(uriString) {
+	const components = {};
+
+	const matches = uriString.match(URI_PARSE);
+
+	if (matches) {
+		if (NO_MATCH_IS_UNDEFINED) {
+			//store each component
+			components.scheme = matches[1];
+			components.userinfo = matches[3];
+			components.host = matches[4];
+			components.port = parseInt(matches[5], 10);
+			components.path = matches[6] || "";
+			components.query = matches[7];
+			components.fragment = matches[8];
+
+			//fix port number
+			if (isNaN(components.port)) {
+				components.port = matches[5];
+			}
+		} else {
+			//IE FIX for improper RegExp matching
+			//store each component
+			components.scheme = matches[1] || undefined;
+			components.userinfo =
+				uriString.indexOf("@") !== -1 ? matches[3] : undefined;
+			components.host = uriString.indexOf("//") !== -1 ? matches[4] : undefined;
+			components.port = parseInt(matches[5], 10);
+			components.path = matches[6] || "";
+			components.query = uriString.indexOf("?") !== -1 ? matches[7] : undefined;
+			components.fragment =
+				uriString.indexOf("#") !== -1 ? matches[8] : undefined;
+
+			//fix port number
+			if (isNaN(components.port)) {
+				components.port = uriString.match(/\/\/(?:.|\n)*:(?:\/|\?|#|$)/)
+					? matches[4]
+					: undefined;
+			}
+		}
+	} else {
+		components.error = components.error || "URI can not be parsed.";
+	}
+
+	return components;
+}
+
 module.exports = {
 	Uri,
 };
