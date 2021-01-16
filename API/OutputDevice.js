51d1d255aa98821e0b6a142ea686884687bbc97d
Ethan Snow <bombitmanbomb@gmail.com>
2021-01-15 21:28:52 -0500

    🚀 RELEASE: 1.10.0

--- API/OutputDevice.js
+++ API/OutputDevice.js
@@ -1,10 +1,5 @@
 const { Enumerable } = require("./Enumerable");
-const OutputDevice = new Enumerable([
-	"Unknown",
-	"Headless",
-	"Screen",
-	"VR",
-]);
+const OutputDevice = new Enumerable(["Unknown", "Headless", "Screen", "VR"]);
 module.exports = {
 	OutputDevice,
 };
