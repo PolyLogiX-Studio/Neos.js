f55fe6c4c3351177773d3c55860d646f743e5506
Ethan Snow <bombitmanbomb@gmail.com>
2021-01-18 12:29:16 -0500

    👌 IMPROVE: ThumbnailInfo

--- API/ThumbnailInfo.js
+++ API/ThumbnailInfo.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require("uuid");
 class ThumbnailInfo {
 	/**
 	 *Creates an instance of ThumbnailInfo.
@@ -9,9 +10,24 @@ class ThumbnailInfo {
 	 */
 	constructor($b) {
 		if (!$b) $b = {};
-		this.MAX_THUMBNAIL_LIFETIME_MINUTES = 10;
 		this.Id = $b.id;
+		this.UploaderIP = $b.uploaderIP;
+		this.SessionId = $b.sessionId;
+		this.UploaderOwnerId = $b.uploaderOwnerId;
 		this.Key = $b.key || null;
+		this.VERSION_KEY = "-v2";
+		this.VERSION = 1;
+	}
+	get IsVersion2() {
+		return ThumbnailInfo.IsIdVersion2(this.Id);
+	}
+
+	static GenerateID(version) {
+		return uuidv4().toString() + (version > 0 ? "-v2" : "") + ".webp";
+	}
+
+	static IsIdVersion2(id) {
+		return id != null && id.Contains("-v2");
 	}
 }
 module.exports = {
