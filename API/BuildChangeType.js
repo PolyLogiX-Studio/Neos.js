929a971bf0b9ff65788df5a5fbd6b625038eebbc
Ethan Snow <bombitmanbomb@gmail.com>
2021-03-13 12:59:20 -0500

    📦 NEW: BuildChangeType

---
+++ API/BuildChangeType.js
@@ -0,0 +1,21 @@
+const { Enumerable } = require("./Enumerable");
+/**
+ * Enumberable for BuildChangeType
+ * @readonly
+ * @enum {Enumerable<string>} BuildChangeType
+ * @property {"NewFeature"} NewFeature
+ * @property {"Tweak"} Tweak
+ * @property {"Optimization"} Optimization
+ * @property {"Security"} Security
+ * @property {"Bugfix"} Bugfix
+ */
+const BuildChangeType = new Enumerable([
+	"NewFeature",
+	"Tweak",
+	"Optimization",
+	"Security",
+	"Bugfix",
+]);
+module.exports = {
+	BuildChangeType,
+};
