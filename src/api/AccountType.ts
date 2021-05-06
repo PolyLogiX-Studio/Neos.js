f459ef31a69fc5a6235deca8babd8b20d9005634
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-06 18:19:18 -0400

    Create AccountType.ts

---
+++ src/api/AccountType.ts
@@ -0,0 +1,19 @@
+/**
+ * Account Type Enumerable.
+ *
+ * @enum {number}
+ */
+export enum AccountType {
+	Normal,
+	AgentSmith,
+	BladeRunner,
+	Gunter,
+	Neuromancer,
+	Architect,
+	Curator,
+	Level144,
+	Level250,
+	Anorak,
+	Level750,
+	END,
+}
