929f31772ca336562b1ca9f083596b7ff4edfb56
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-19 03:21:05 -0400

    Update AccountType.ts

--- src/api/AccountType.ts
+++ src/api/AccountType.ts
@@ -4,16 +4,16 @@
  * @enum {number}
  */
 export enum AccountType {
-	Normal,
-	AgentSmith,
-	BladeRunner,
-	Gunter,
-	Neuromancer,
-	Architect,
-	Curator,
-	Level144,
-	Level250,
-	Anorak,
-	Level750,
-	END,
+	Normal = "Normal",
+	AgentSmith = "AgentSmith",
+	BladeRunner = "BladeRunner",
+	Gunter = "Gunter",
+	Neuromancer = "Neuromancer",
+	Architect = "Architect",
+	Curator = "Curator",
+	Level144 = "Level144",
+	Level250 = "Level250",
+	Anorak = "Anorak",
+	Level750 = "Level750",
+	END = "END",
 }
