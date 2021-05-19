e47d287d3dfc2f02d300f84d909bdd52289b9952
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-19 03:21:26 -0400

    Create AssetUploadData.ts

---
+++ src/api/AssetUploadData.ts
@@ -0,0 +1,39 @@
+import type { UploadState } from "./UploadState";
+export class AssetUploadData {
+	public Signature: string;
+	public Variant: string;
+	public OwnerId: string;
+	public TotalBytes: number;
+	public ChunkSize: number;
+	public TotalChunks: number;
+	public UploadState: UploadState;
+	constructor($b: AssetUploadDataJSON) {
+		this.Signature = $b.signature;
+		this.Variant = $b.variant;
+		this.OwnerId = $b.ownerId;
+		this.TotalBytes = $b.totalBytes;
+		this.ChunkSize = $b.chunkSize;
+		this.TotalChunks = $b.totalChunks;
+		this.UploadState = $b.uploadState;
+	}
+	toJSON(): AssetUploadDataJSON {
+		return {
+			signature: this.Signature,
+			variant: this.Variant,
+			ownerId: this.OwnerId,
+			totalBytes: this.TotalBytes,
+			chunkSize: this.ChunkSize,
+			totalChunks: this.TotalChunks,
+			uploadState: this.UploadState,
+		};
+	}
+}
+interface AssetUploadDataJSON {
+	signature: string;
+	variant: string;
+	ownerId: string;
+	totalBytes: number;
+	chunkSize: number;
+	totalChunks: number;
+	uploadState: UploadState;
+}
