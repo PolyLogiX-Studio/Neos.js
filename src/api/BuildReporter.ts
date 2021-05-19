455a14a2d3b80fa1b2690a60946e67872b0492eb
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-19 04:19:31 -0400

    Create BuildReporter.ts

---
+++ src/api/BuildReporter.ts
@@ -0,0 +1,26 @@
+export class BuildReporter {
+	public Username: string;
+	public NeosUserID: string;
+	public DiscordHandle: string;
+	public GithubHandle: string;
+	constructor($b: BuildReporterJSON) {
+		this.Username = $b.username;
+		this.NeosUserID = $b.neosUserID;
+		this.DiscordHandle = $b.discordHandle;
+		this.GithubHandle = $b.githubHandle;
+	}
+	toJSON(): BuildReporterJSON {
+		return {
+			username: this.Username,
+			neosUserID: this.NeosUserID,
+			discordHandle: this.DiscordHandle,
+			githubHandle: this.GithubHandle,
+		};
+	}
+}
+interface BuildReporterJSON {
+	username: string;
+	neosUserID: string;
+	discordHandle: string;
+	githubHandle: string;
+}
