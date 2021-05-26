32f6324f6d4e48eb0d2edab0acb9b0e529fea1c8
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-26 02:16:34 -0400

    Update SessionInfo.ts

--- src/api/SessionInfo.ts
+++ src/api/SessionInfo.ts
@@ -1,6 +1,7 @@
+import { RecordId } from "./RecordId";
 export class SessionInfo {}
 export interface SessionInfoJSON {
 	name: string;
 	description: string;
-	correspondingWorldId: Record;
+	correspondingWorldId: RecordId;
 }
