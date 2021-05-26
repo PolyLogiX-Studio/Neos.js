67dfeb0980aef4b614b82ee13695138f40ceb908
Ethan Snow <bombitmanbomb@gmail.com>
2021-05-25 20:49:48 -0400

    Update IRecord.ts

--- src/api/IRecord.ts
+++ src/api/IRecord.ts
@@ -1,27 +1,27 @@
 import type { List } from "@bombitmanbomb/utils";
 import type { NeosDBAsset } from "./NeosDBAsset";
 export interface IRecord {
-	RecordId: string;
-	OwnerId: string;
-	AssetURI: string;
-	GlobalVersion: number;
-	LocalVersion: number;
-	LastModifyingUserId: string;
-	LastModifyingMachineId: string;
-	Name: string;
-	OwnerName: string;
+	RecordId: string; //
+	OwnerId: string; //
+	AssetURI?: string;
+	GlobalVersion?: number;
+	LocalVersion?: number;
+	LastModifyingUserId?: string;
+	LastModifyingMachineId?: string;
+	Name?: string; //
+	OwnerName?: string;
 	Description?: string;
-	RecordType: string;
+	RecordType: string; //
 	Tags?: string[];
-	Path: string;
+	Path?: string;
 	ThumbnailURI?: string;
-	IsPublic: boolean;
-	IsForPatrons: boolean;
-	IsListed: boolean;
-	Visits: number;
-	Rating: number;
+	IsPublic?: boolean;
+	IsForPatrons?: boolean;
+	IsListed?: boolean;
+	Visits?: number;
+	Rating?: number;
 	FirstPublishTime?: Date;
 	CreationTime?: Date;
-	LastModificationTime: Date;
+	LastModificationTime?: Date;
 	NeosDBManifest?: List<NeosDBAsset>;
 }
